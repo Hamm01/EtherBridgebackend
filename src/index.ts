ca69054a08c661c6fbec0d1debee23fb7275e5ed
Himanish kochhar <himanish.kochhar@gmail.com>
2024-09-24 09:04:14 +0530

    added poll block function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { JsonRpcProvider, id } from 'ethers'
 
 const provider = new JsonRpcProvider(
-  'https://eth-mainnet.g.alchemy.com/v2/OIoR91FXtiqdHtEnysgoYYQGXlv'
+  'https://eth-mainnet.g.alchemy.com/v2/OIoR91FXtiqdHtEnysgoYQGXlv'
 )
+
+async function pollBlock(blockNumber: number) {
+  const logs = await provider.getLogs({
+    address: '0xdac17f958d2ee523a2206206994597c13d831ec7', //this is usdt contract
+    fromBlock: blockNumber,
+    toBlock: blockNumber,
+    topics: [id('Transfer(address,address,uint256)')]
+  })
+}
